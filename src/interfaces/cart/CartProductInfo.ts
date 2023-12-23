332774f4a126ebac8bb3976a106291c2dcec687a
Dmytrych <55029343+Dmytrych@users.noreply.github.com>
2023-12-23 11:44:15 +0200

    another type fix

--- src/interfaces/cart/CartProductInfo.ts
+++ src/interfaces/cart/CartProductInfo.ts
@@ -1,5 +1,5 @@
-import {ICartSelection} from "@/contexts/CartContext";
 import {Product} from "@/api/DTO/products";
+import {ICartSelection} from "@/contexts/CartStateContext";
 
 export interface CartProductInfo extends ICartSelection {
   product: Product;
