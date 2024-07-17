db369a2f900557a9c963dea5a2c21717d5276ea2
Dmytro Habaznia <55029343+Dmytrych@users.noreply.github.com>
2024-07-17 22:47:39 +0300

    Moved components

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,5 +1,112 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ProductDataCharacteristics extends Schema.Component {
+  collectionName: 'components_product_data_characteristics';
+  info: {
+    displayName: 'Characteristics';
+    icon: 'puzzle';
+    description: '';
+  };
+  attributes: {
+    name: Attribute.String & Attribute.Required;
+    value: Attribute.Text & Attribute.Required;
+  };
+}
+
+export interface OrderInfoOrderItem extends Schema.Component {
+  collectionName: 'components_order_info_order_items';
+  info: {
+    displayName: 'OrderItem';
+    icon: 'shoppingCart';
+    description: '';
+  };
+  attributes: {
+    product: Attribute.Relation<
+      'order-info.order-item',
+      'oneToOne',
+      'api::product.product'
+    >;
+    quantity: Attribute.Integer &
+      Attribute.Required &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+        },
+        number
+      > &
+      Attribute.DefaultTo<1>;
+    price: Attribute.Decimal &
+      Attribute.Required &
+      Attribute.SetMinMax<
+        {
+          min: 0.001;
+        },
+        number
+      >;
+  };
+}
+
+export interface OrderInfoDeliveryInfo extends Schema.Component {
+  collectionName: 'components_order_info_delivery_infos';
+  info: {
+    displayName: 'DeliveryInfo';
+  };
+  attributes: {
+    deliveryLocation: Attribute.Text &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 6;
+      }>;
+  };
+}
+
+export interface InfoBlocksTextWithPicture extends Schema.Component {
+  collectionName: 'components_info_blocks_text_with_pictures';
+  info: {
+    displayName: 'text-with-picture';
+    icon: 'code';
+    description: '';
+  };
+  attributes: {
+    text: Attribute.RichText & Attribute.Required;
+    image: Attribute.Media<'images'> & Attribute.Required;
+    variant: Attribute.Enumeration<['pictureRight', 'pictureLeft']> &
+      Attribute.Required &
+      Attribute.DefaultTo<'pictureRight'>;
+  };
+}
+
+export interface PersonalInfoCustomerInfo extends Schema.Component {
+  collectionName: 'components_personal_info_customer_infos';
+  info: {
+    displayName: 'CustomerInfo';
+    icon: 'user';
+    description: '';
+  };
+  attributes: {
+    email: Attribute.Email;
+    name: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 2;
+      }>;
+    surname: Attribute.String &
+      Attribute.Required &
+      Attribute.SetMinMaxLength<{
+        minLength: 2;
+      }>;
+    phoneNumber: Attribute.String & Attribute.Required;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'product-data.characteristics': ProductDataCharacteristics;
+      'order-info.order-item': OrderInfoOrderItem;
+      'order-info.delivery-info': OrderInfoDeliveryInfo;
+      'info-blocks.text-with-picture': InfoBlocksTextWithPicture;
+      'personal-info.customer-info': PersonalInfoCustomerInfo;
+    }
+  }
 }
--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -788,6 +788,116 @@ export interface PluginI18NLocale extends Schema.CollectionType {
   };
 }
 
+export interface ApiHomePageHomePage extends Schema.SingleType {
+  collectionName: 'home_pages';
+  info: {
+    singularName: 'home-page';
+    pluralName: 'home-pages';
+    displayName: 'Home Page';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    blocks: Attribute.DynamicZone<['info-blocks.text-with-picture']>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::home-page.home-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::home-page.home-page',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiOrderOrder extends Schema.CollectionType {
+  collectionName: 'orders';
+  info: {
+    singularName: 'order';
+    pluralName: 'orders';
+    displayName: 'Order';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    customerInfo: Attribute.Component<'personal-info.customer-info'> &
+      Attribute.Required;
+    deliveryInfo: Attribute.Component<'order-info.delivery-info'> &
+      Attribute.Required;
+    items: Attribute.Component<'order-info.order-item', true>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::order.order',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
+export interface ApiProductProduct extends Schema.CollectionType {
+  collectionName: 'products';
+  info: {
+    singularName: 'product';
+    pluralName: 'products';
+    displayName: 'Product';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    description: Attribute.RichText;
+    name: Attribute.String & Attribute.Required;
+    images: Attribute.Media<'images', true> & Attribute.Required;
+    code: Attribute.UID & Attribute.Required;
+    shortDescription: Attribute.Text & Attribute.Required;
+    price: Attribute.Decimal &
+      Attribute.Required &
+      Attribute.SetMinMax<
+        {
+          min: 0.001;
+        },
+        number
+      >;
+    characteristics: Attribute.Component<'product-data.characteristics', true>;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::product.product',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::product.product',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 declare module '@strapi/types' {
   export module Shared {
     export interface ContentTypes {
@@ -806,6 +916,9 @@ declare module '@strapi/types' {
       'plugin::users-permissions.role': PluginUsersPermissionsRole;
       'plugin::users-permissions.user': PluginUsersPermissionsUser;
       'plugin::i18n.locale': PluginI18NLocale;
+      'api::home-page.home-page': ApiHomePageHomePage;
+      'api::order.order': ApiOrderOrder;
+      'api::product.product': ApiProductProduct;
     }
   }
 }
