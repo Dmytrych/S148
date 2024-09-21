ef68bdd8847b1b8ab3ac9567f1433b63fe5c7d79
Dmytro Khabaznia <d.khabaznia@KIE-X-L-0746.pt.playtech.corp>
2024-09-21 13:50:11 +0300

    Twitter removal

--- front-end/src/components/Footer/components/Socials.tsx
+++ front-end/src/components/Footer/components/Socials.tsx
@@ -1,9 +1,8 @@
 import {IconButton, Stack} from "@mui/material";
-import {Facebook, Instagram, Twitter} from "@mui/icons-material";
+import {Facebook, Instagram} from "@mui/icons-material";
 
 const SOCIAL_MEDIA_LINKS = {
   facebook: process.env.NEXT_PUBLIC_FACEBOOK_LINK ?? "",
-  twitter: process.env.NEXT_PUBLIC_TWITTER_LINK ?? "",
   instagram: process.env.NEXT_PUBLIC_INSTAGRAM_LIMK ?? "",
 };
 
@@ -17,14 +16,6 @@ export function Socials() {
         rel="noopener noreferrer"
       >
         <Facebook color="primary" transform="scale(1.5)" />
-      </IconButton>
-      <IconButton
-        size="large"
-        href={SOCIAL_MEDIA_LINKS.twitter}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Twitter color="primary" transform="scale(1.5)" />
       </IconButton>
       <IconButton
         size="large"
