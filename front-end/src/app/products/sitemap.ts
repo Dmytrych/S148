9aa31ba8f493234928c72982132de3b19bcf08c6
Dmytro Khabaznia <d.khabaznia@KIE-X-L-0746.pt.playtech.corp>
2024-09-21 14:34:12 +0300

    Update sitemap.ts

--- front-end/src/app/products/sitemap.ts
+++ front-end/src/app/products/sitemap.ts
@@ -1,7 +1,25 @@
 import {MetadataRoute} from "next";
 import {fetchProducts} from "@/actions/fetchProducts";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export async function generateSitemaps() {
+  const products = await fetchProducts()
+
+  if (!products) {
+    return []
+  }
+
+  return products.map((product) => {
+    return {
+      id: product.id
+    }
+  })
+}
+
+export default async function sitemap({
+  id,
+}: {
+  id: number
+}): Promise<MetadataRoute.Sitemap> {
   const products = await fetchProducts()
 
   if (!products) {
