462e33987422c7845613fb6a8d27f24f6bfe5a61
Dmytro Khabaznia <d.khabaznia@KIE-X-L-0746.pt.playtech.corp>
2024-10-08 23:14:33 +0300

    Added data revalidation

--- front-end/src/app/articles/page.tsx
+++ front-end/src/app/articles/page.tsx
@@ -4,10 +4,11 @@ import AdaptiveCardGrid from "@/components/articles/AdaptiveCardGrid";
 import ArticleCard from "@/components/articles/ArticleCard";
 import {locale} from "@/locale/ua";
 import {Routes} from "@/routes";
-import {notFound} from "next/navigation";
 import {Box, Container, Typography} from "@mui/material";
 import {getOptimizedImageUrl} from "@/helpers/product/get-optimized-image-url";
 import {getImageUrl} from "@/helpers/image-url";
+
+export const revalidate = 60
 
 async function Page() {
   const articles = await fetchArticles<ArticleAttributes>(['title', 'slug', 'createdAt', 'description'], ['coverImage'])
@@ -15,9 +16,6 @@ async function Page() {
   return (<Container sx={{ pt: 3 }}>
     <Box display="flex" justifyContent="center" mb={2}>
       <Typography variant="h5_squares">{locale.articles_page}</Typography>
-    </Box>
-    <Box>
-      {JSON.stringify(articles)}
     </Box>
     { articles ? <AdaptiveCardGrid items={articles} renderItem={(article) => (
       <ArticleCard
