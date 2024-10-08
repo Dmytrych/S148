ccf2911eca685393d9a157d3a8748797b1b10012
Dmytro Khabaznia <d.khabaznia@KIE-X-L-0746.pt.playtech.corp>
2024-10-08 22:59:23 +0300

    Fix

--- front-end/src/app/articles/page.tsx
+++ front-end/src/app/articles/page.tsx
@@ -5,23 +5,18 @@ import ArticleCard from "@/components/articles/ArticleCard";
 import {locale} from "@/locale/ua";
 import {Routes} from "@/routes";
 import {notFound} from "next/navigation";
-import {Box, Container, Divider, Typography} from "@mui/material";
+import {Box, Container, Typography} from "@mui/material";
 import {getOptimizedImageUrl} from "@/helpers/product/get-optimized-image-url";
 import {getImageUrl} from "@/helpers/image-url";
 
 async function Page() {
-
   const articles = await fetchArticles<ArticleAttributes>(['title', 'slug', 'createdAt', 'description'], ['coverImage'])
-
-  if (!articles) {
-    return notFound()
-  }
 
   return (<Container sx={{ pt: 3 }}>
     <Box display="flex" justifyContent="center" mb={2}>
       <Typography variant="h5_squares">{locale.articles_page}</Typography>
     </Box>
-    <AdaptiveCardGrid items={articles} renderItem={(article) => (
+    { articles ? <AdaptiveCardGrid items={articles} renderItem={(article) => (
       <ArticleCard
         imageSrc={getImageUrl(getOptimizedImageUrl(article.attributes.coverImage.data, 'small')) ?? ""}
         altText={article.attributes.coverImage.data.attributes.alternativeText}
@@ -29,7 +24,7 @@ async function Page() {
         title={article.attributes.title}
         description={article.attributes.description}
       />
-    )}/>
+    )}/> : null}
   </Container>)
 }
 
