f7f83498eb3ae1bae8eb40d24d52fc3687fe6916
Dmytro Habaznia <karambolrul@gmail.com>
2024-10-16 23:27:37 +0300

    Added telegram socials

--- front-end/src/components/Footer/components/Socials.tsx
+++ front-end/src/components/Footer/components/Socials.tsx
@@ -1,14 +1,23 @@
 import {IconButton, Stack} from "@mui/material";
-import {Facebook, Instagram} from "@mui/icons-material";
+import {Facebook, Instagram, Telegram} from "@mui/icons-material";
 
 const SOCIAL_MEDIA_LINKS = {
   facebook: process.env.NEXT_PUBLIC_FACEBOOK_LINK ?? "",
   instagram: process.env.NEXT_PUBLIC_INSTAGRAM_LINK ?? "",
+  telegram: process.env.NEXT_PUBLIC_TELEGRAM_LINK ?? ""
 };
 
 export function Socials() {
   return (
-    <Stack direction="row" justifyContent="center">
+    <Stack direction="row" justifyContent="center" gap={2}>
+      <IconButton
+        size="large"
+        href={SOCIAL_MEDIA_LINKS.telegram}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Telegram color="primary" transform="scale(1.5)" />
+      </IconButton>
       <IconButton
         size="large"
         href={SOCIAL_MEDIA_LINKS.facebook}
--- front-end/src/components/articles/ArticleContent.tsx
+++ front-end/src/components/articles/ArticleContent.tsx
@@ -4,7 +4,6 @@ import {MDXRemote} from "next-mdx-remote/rsc";
 import {Box} from "@mui/material";
 import ArticleImage from "@/components/articles/ArticleImage";
 import {ErrorBoundary} from "react-error-boundary";
-import {locale} from "@/locale/ua";
 
 type ArticleContentProps = {
   content: string;
